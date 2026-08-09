fb07aec904012bbdf048fce039240f097b0125fe
Megan Tan <meganj.tan@gmail.com>
2026-08-09 17:45:25 -0400

    added basiccalculator specification and implementation

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -4,3 +4,36 @@
 // Module 10 - Final Project
 // Class Implementation - Basic Calculator
 
+#include "BasicCalculator.h"
+using namespace std;
+
+BasicCalculator::BasicCalculator(){
+    this->operand1 = 0.0;
+    this->operand2 = 0.0;
+    this->result = 0.0;
+    strncpy(this->expression, "Null", 50);
+}
+
+BasicCalculator::BasicCalculator(double operand1, double operand2, double result, const char* expression){
+    this->operand1 = operand1;
+    this->operand2 = operand2;
+    this->result = result;
+    strncpy(this->expression, expression, 50);
+}
+
+void BasicCalculator::setOperand1(double operand1) { this->operand1 = operand1; }
+void BasicCalculator::setOperand2(double operand2){ this->operand2 = operand2; }
+void BasicCalculator::setResult(double result) { this->result = result; }
+void BasicCalculator::setExpression(const char* expression){
+    strncpy(this->expression, expression, 49);
+    this->expression[49]='\0';
+}
+
+double BasicCalculator::getOperand1() const{ return operand1; }
+double BasicCalculator::getOperand2() const{ return operand2; }
+double BasicCalculator::getResult() const { return result; }
+const char* BasicCalculator::getExpression() const { return expression; }
+
+void BasicCalculator::displayInfo(){
+    
+}
--- BasicCalculator.h
+++ BasicCalculator.h
@@ -7,4 +7,31 @@
 #ifndef BASIC_CALCULATOR_H
 #define BASIC_CALCULATOR_H
 
+#include <cstring>
+using namespace std;
+
+class BasicCalculator{
+    private: 
+        double operand1;
+        double operand2;
+        double result;
+        char expression[50];
+
+    public: 
+        BasicCalculator();
+        BasicCalculator(double operand1, double operand2, double result, const char* expression);
+
+        void setOperand1(double operand1);
+        void setOperand2(double operand2);
+        void setResult(double result);
+        void setExpression(const char* expression);
+
+        double getOperand1() const;
+        double getOperand2() const;
+        double getResult() const;
+        const char* getExpression() const;
+
+        virtual void displayInfo();
+};
+
 #endif 
